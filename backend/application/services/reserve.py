ac8baef17cc207a1eb6c3a97ec7c1f1a5427905f
hangzhou188 <1162951519@qq.com>
2021-11-23 21:38:48 +0800

    修复取消预约的bug

--- backend/application/services/reserve.py
+++ backend/application/services/reserve.py
@@ -56,6 +56,8 @@ class ReserveService:
         """
         query_reserve_record = Reserve_Record.query.filter(Reserve_Record.userID==userID,
                                                             Reserve_Record.recordID==recordID).first()
+        if query_reserve_record==None:
+            return '无匹配记录',False
         if strict:
             now = datetime.datetime.now()
             reserveDate = query_reserve_record.reserveDate
@@ -64,8 +66,6 @@ class ReserveService:
                                                 startTime.hour, startTime.minute, startTime.second)
             if target_startTime<=now:
                 return '超时，不可取消',False
-        if query_reserve_record==None:
-            return '无匹配记录',False
         try:
             db.session.delete(query_reserve_record)
             db.session.commit()
