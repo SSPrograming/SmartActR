9359a0cdd4bb2361a2eb3be50361649ffdd9ef1e
hangzhou188 <1162951519@qq.com>
2021-11-23 23:28:00 +0800

    hello world: manager_backend

--- manager_backend/manage.py
+++ manager_backend/manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from config import query_yaml
 from application import db
-
+from application.api import bp
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = query_yaml('db.MYSQL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -12,7 +12,12 @@ db.init_app(app)
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+for bluep in bp:
+    app.register_blueprint(bluep)
+
 manager.add_command('db', MigrateCommand)
+
+
 
 if __name__ == '__main__':
     manager.run()
