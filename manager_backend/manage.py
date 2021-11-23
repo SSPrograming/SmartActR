02a43b108e16e1f7d4ae24fb02e28c6a482a988c
hangzhou188 <1162951519@qq.com>
2021-11-23 21:34:33 +0800

    初始化manager_backend

---
+++ manager_backend/manage.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from config import query_yaml
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = query_yaml('db.MYSQL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db.init_app(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
