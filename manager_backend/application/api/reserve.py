5346b60cf6879d048f5ebf5c6647ef55bec4c3ab
hangzhou188 <1162951519@qq.com>
2021-11-26 20:54:36 +0800

    查看历史预约记录接口

--- manager_backend/application/api/reserve.py
+++ manager_backend/application/api/reserve.py
@@ -29,3 +29,30 @@ def getTodayRecord():
 			"equipmentName": EquipmentService.get_name_by_type(item.equipmentType)[0]
         })
     return jsonify({"errCode": 0, "recordList": record_list}), 200
+
+
+@bp_reserve.route('/api/v1/reserve/getHistoryRecord', methods=['POST'])
+@login_required
+def getHistoryRecord():
+    try:
+        start_date = request.json["startDate"]
+        end_date = request.json["endDate"]
+    except:
+        return jsonify({"errCode": 1, "errMsg": "bad arguments"}), 200
+    try:
+        record_list_raw = ReserveService.get_history_record(start_date, end_date)
+    except:
+        return jsonify({"errCode": 1, "errMsg": "bad arguments"}), 200
+    record_list = []
+    for item in record_list_raw:
+        record_list.append({
+            "recordID": item.recordID,
+			"postTime": item.postTime.strftime("%Y-%m-%d %H:%M") if item.postTime else "undefined",
+			"reserveDate": item.reserveDate.strftime("%Y-%m-%d") if item.reserveDate else "undefined",
+			"startTime": item.startTime.strftime("%H:%M") if item.startTime else "undefined",
+			"endTime": item.endTime.strftime("%H:%M") if item.endTime else "undefined",
+			"userName": UserService.get_name_by_id(item.userID),
+			"status": item.status,
+			"equipmentName": EquipmentService.get_name_by_type(item.equipmentType)[0]
+        })
+    return jsonify({"errCode": 0, "recordList": record_list}), 200
--- manager_backend/application/service/reserve.py
+++ manager_backend/application/service/reserve.py
@@ -9,3 +9,10 @@ class ReserveService():
         now_date = datetime.date(now_datetime.year, now_datetime.month, now_datetime.day)
         record_list = Reserve_Record.query.filter(Reserve_Record.reserveDate==now_date).all()
         return record_list
+
+    def get_history_record(start_date_raw, end_date_raw):
+        start_date = strToDate(start_date_raw)
+        end_date = strToDate(end_date_raw)
+        record_list = Reserve_Record.query.filter(Reserve_Record.reserveDate>=start_date,
+                                                  Reserve_Record.reserveDate<=end_date).all()
+        return record_list
