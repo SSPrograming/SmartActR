fe7fc278b0bee27edfb11a3655e1581c3fa575ef
wengzt <281810067@qq.com>
2021-12-09 12:00:15 +0800

    debug

--- backend/application/apis/__init__.py
+++ backend/application/apis/__init__.py
@@ -1,3 +1,3 @@
-from application.apis import user, reserve, notice,checkIn
+from application.apis import user, reserve, notice,checkIn, freeze
 
-bp = [user.bp_user,reserve.bp_reserve,notice.bp_notice,checkIn.bp_checkIn]
+bp = [user.bp_user,reserve.bp_reserve,notice.bp_notice,checkIn.bp_checkIn,freeze.bp_freeze]
