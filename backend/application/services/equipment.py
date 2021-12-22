c5ded368e2df8c0e3b70fca66e783f17688224d2
hangzhou188 <1162951519@qq.com>
2021-12-22 22:42:23 +0800

    代码格式化以及SSL配置

--- backend/application/services/equipment.py
+++ backend/application/services/equipment.py
@@ -1,6 +1,7 @@
 from application.database import db
 from application.models import Equipment, equipmentType
 
+
 class EquipmentService:
     def get_all_equipmentType():
         """
@@ -9,11 +10,13 @@ class EquipmentService:
         return equipmentType.query.all()
 
     def get_all_equipment():
-        EquipmentTypeList = equipmentType.query.order_by(equipmentType.equipmentOrder).all()
+        EquipmentTypeList = equipmentType.query.order_by(
+            equipmentType.equipmentOrder).all()
         EquipmentList = []
         for Type in EquipmentTypeList:
             TypeID = Type.equipmentType
-            m_equipmentList = Equipment.query.filter(Equipment.equipmentType==TypeID).all()
+            m_equipmentList = Equipment.query.filter(
+                Equipment.equipmentType == TypeID).all()
             EquipmentList.extend(m_equipmentList)
         return EquipmentList
 
@@ -21,21 +24,22 @@ class EquipmentService:
         """
         ret None if the ID does not exist
         """
-        return equipmentType.query.filter(equipmentType.equipmentType==equipmentTypeID).first()
+        return equipmentType.query.filter(equipmentType.equipmentType == equipmentTypeID).first()
 
     def get_single_equipment(equipmentTypeID, equipmentID):
         """
         ret None if the equipment does not exist
         """
-        return Equipment.query.filter(Equipment.equipmentType==equipmentTypeID,
-                                      Equipment.equipmentID==equipmentID).first()
-    
+        return Equipment.query.filter(Equipment.equipmentType == equipmentTypeID,
+                                      Equipment.equipmentID == equipmentID).first()
+
     def insert_single_equipmentType(equipmentName, equipmentDescription):
         """
         插入单种设备
         """
-        existedName = equipmentType.query.filter(equipmentType.equipmentName==equipmentName).first()
-        if existedName is None: # 确保不会重名造成歧义
+        existedName = equipmentType.query.filter(
+            equipmentType.equipmentName == equipmentName).first()
+        if existedName is None:  # 确保不会重名造成歧义
             newType = equipmentType(equipmentName=equipmentName,
                                     equipmentDescription=equipmentDescription)
             try:
@@ -53,8 +57,8 @@ class EquipmentService:
         """
         插入单台设备
         """
-        newEquipment = Equipment(equipmentType=equipmentTypeID,equipmentID=equipmentID,
-                                equipmentStatus='完好')
+        newEquipment = Equipment(equipmentType=equipmentTypeID, equipmentID=equipmentID,
+                                 equipmentStatus='完好')
         try:
             db.session.add(newEquipment)
             db.session.commit()
@@ -63,9 +67,10 @@ class EquipmentService:
             print(e)
             db.session.rollback()
             return False
-    
+
     def get_name_by_Type(Type):
-        target_type = equipmentType.query.filter(equipmentType.equipmentType==Type).first()
+        target_type = equipmentType.query.filter(
+            equipmentType.equipmentType == Type).first()
         if target_type is None:
             return "不存在的设备"
         else:
--- backend/application/services/notice.py
+++ backend/application/services/notice.py
@@ -1,6 +1,7 @@
 from application.database import db
 from application.models import TableNotcie
 
+
 class NoticeService:
     def get_notice():
         return TableNotcie.query.order_by(TableNotcie.noticeID.desc()).first()
